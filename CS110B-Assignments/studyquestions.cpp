f143dc0c70082a079067dafbaf4d9cd09f2d4938
Victoria <victoria.022@proton.me>
2022-10-03 09:45:57 -0700

    Midterm 1 study guide & exercises

---
+++ CS110B-Assignments/studyquestions.cpp
@@ -0,0 +1,14 @@
+//Study questions: Victoria Barnett
+#include <iostream>
+
+using namespace std;
+
+int countMult(const int array[], int size);
+
+int main() {
+    int cards[3];
+    cards[1] = 2;
+    cards[2] = 4;
+    cards[3] = 7;
+    
+}
